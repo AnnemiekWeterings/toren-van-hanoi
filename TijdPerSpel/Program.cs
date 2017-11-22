f894c8ad07e9ba6ddd3b0093cb711e7dfc79466d
Annemiek Weterings <1994annemiek@gmail.com>
2017-11-22 16:40:58 +0100

    TIjden nu tot millenia

--- TijdPerSpel/Program.cs
+++ TijdPerSpel/Program.cs
@@ -26,7 +26,7 @@ namespace TijdPerSpel
 
         static private ulong CalculateTurn(ulong output, int schijven, int herhalingen)
         {
-            if (herhalingen < schijven - 1)
+             if (herhalingen < schijven - 1)
             {
                 output *= 2;
                 herhalingen += 1;
@@ -35,18 +35,43 @@ namespace TijdPerSpel
 
             else
             {
+                // PRBLM: Dit kan mogelijk mis g 
                 return output - 1;
             }
         }
 
         static private string CalculateTime(ulong sec)
         {
-            ulong myHour = sec / 3600;
-            ulong myMin = sec % 3600 / 60;
-            ulong mySec = (sec % 3600) % 60;
-            string myTime = string.Format("{0:D2}:{1:D2}:{2:D2}", myHour, myMin, mySec);
+            /*  ulong myDay = sec / 86400;
+                ulong myHour = sec % 86400 / 3600;
+                ulong myMin = sec % 3600 / 60;
+                ulong mySec = (sec % 3600) % 60;
+                string myTime = string.Format("{0:D2} dagen {1:D2}:{2:D2}:{3:D2}", myDay, myHour, myMin, mySec);
 
-            return myTime;
+                return myTime;
+            */
+
+            ulong day = 86400;
+            ulong year = day * 365;
+            ulong decenia = year * 10;
+            ulong century = decenia * 10;
+            ulong millenium = century * 10;
+
+            ulong myMillenium = sec / millenium;
+            ulong myCentury = sec % millenium / century;
+            ulong myDecenia = sec % millenium % century / decenia;
+            ulong myYear = sec % millenium % century % decenia / year;
+            ulong myDay = sec % millenium % century % decenia % year / day;
+            ulong myHour = sec % millenium % century % decenia % year % day / 3600;
+            ulong myMin = sec % millenium % century % decenia % year % day % 3600 / 60;
+            ulong mySec = sec % millenium % century % decenia % year % day % 3600 % 60 % 60;
+
+
+            // string myTime = string.Format("{0:D2} eeuwen {1:D2}:{2:D2}:{3:D2}", myDay, myHour, myMin, mySec);
+
+            return Convert.ToString(myMillenium + " millenia " + myCentury + " centuries " + myDecenia + " decenia " + myYear + " years " + myDay +" days "+ myHour +":"+ myMin + ":" + mySec);
+            
+            //return Convert.ToString(myMillenium +" millenia "+ myCentury + " eeuwen " + myDecenia + " decenia " + myYear + " jaren " + myDay + "dagen");
         }
     }
 }
